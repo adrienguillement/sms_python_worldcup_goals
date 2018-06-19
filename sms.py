668e97989d70e549ecf31b744c4024de55927608
Adrien Guillement <adrien.guillement@gmail.com>
2018-06-19 11:26:27 +0200

    Update sms.py

--- sms.py
+++ sms.py
@@ -10,7 +10,6 @@ auth_token = ''
 myPhone = '+'
 TwilioNumber = '+'
 
-now = datetime.datetime.now()
 url = "http://worldcup.sfg.io/matches/country?fifa_code=ENG"
 response = urllib.urlopen(url)
 data = json.loads(response.read())
@@ -22,6 +21,7 @@ if os.path.isfile('data.txt'):
   with open('data.txt', 'w') as outfile:
       json.dump(data, outfile)
 
+  now = datetime.datetime.now()
   for i in range(0, len(data_old)):
     if data_old[i]["datetime"].split("T")[0] == now.strftime("%Y-%m-%d"):
       today_id = i
@@ -60,11 +60,7 @@ if os.path.isfile('data.txt'):
         to=myPhone,
         from_=TwilioNumber,
         body="\n{domicile} {domicile_score}-{exterieur_score} {exterieur} \n {domicile} : \n {domicile_detail} \n \n {exterieur} : \n {exterieur_detail}".format(domicile=domicile,
-                                                                                                                                                              exterieur=exterieur,
-                                                                                                                                                              domicile_score=len(domicile_goals),
-                                                                                                                                                              exterieur_score=len(exterieur_goals),
-                                                                                                                                                              domicile_detail=domicile_detail,
-                                                                                                                                                              exterieur_detail=exterieur_detail) + u'\U0001f680')
+                                                                                                                                              exterieur_detail=exterieur_detail) + u'\U0001f680')
   except Exception as e:
     print e
 
